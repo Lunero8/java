20e0326712006b9ddf8cc9e04a4a5d35004f7603
Sayem <sayemsoftd@gmail.com>
2025-07-27 16:24:17 +0600

    strings

---
+++ strings/basic.java
@@ -0,0 +1,66 @@
+/**
+ *    author:  Lunero
+ *    created: 27.07.2025 15:12:34
+**/
+import java.util.* ;
+
+public class basic {
+      public static void main (String[] args) {
+            Scanner sc = new Scanner (System.in) ;
+
+            // String declaration
+            String name = "Sayem" ;
+            String fullName = "Aminur Rahman Sayem" ;
+            String sentence = "My name is Lunero." ;
+
+            // input from user
+            String userName = sc.next() ; // single word
+            String userName2 = sc.nextLine() ; // full sentence
+
+            char a = sc.next().charAt(0) ; // input "sayem" a = 's'
+            char b = sc.next().charAt(1) ; // input "sayem" b = 'a'
+
+            // functions
+            // concatenation
+            String firstName = "Aminur Rahman" ;
+            String lastName = "Sayem" ;
+
+            String fullNameConcatenation = firstName + " " + lastName ; // Aminur Rahman Sayem
+
+            // string length
+            int length = firstName.length() ; // 13
+
+            // .charAt() : select a character from string
+            char d = firstName.charAt(5) ; // 'r' index based
+
+            // .compareTo : compare strings
+            String compareName1 = "Sayem" ;
+            String compareName2 = "Lunero" ;
+
+                  /* 
+                  - s1 > s2 : +ve value
+                  - s1 == s2 : 0
+                  - s1 < s2 : -ve value 
+                  */
+
+            if (compareName1.compareTo (compareName2) == 0){ 
+                  // print equal, greater or smaller 
+            }
+
+            // .substring : Sub String
+            String substringFullName = "Aminur Rahman Sayem" ;
+            String substringFirstName = substringFullName.substring(0, 5) ; // Aminu , (beginning index, last index+1)
+            String substringLastName = substringFullName.substring(5) ; //r Rahman Sayem , (index 5 to last index)
+
+            // string to int 
+            String str = "123" ;
+            int num = Integer.parseInt(str) ; // using Integer.parseInt() (most common)
+            int num2 = Integer.valueOf(str) ; // Using Integer.valueOf() (returns an Integer object)
+
+            // int to string
+            num = 123 ;
+            str = String.valueOf(num) ; // Using String.valueOf()
+            str = Integer.toString(num) ; // Using Integer.toString()
+            str = num + "" ; // Using String Concatenation (+ "")
+      }
+}
