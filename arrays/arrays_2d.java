70be4fb36a3feefc8ecb95bdaae12f5f2b5828ed
Sayem <sayemsoftd@gmail.com>
2025-08-13 01:05:25 +0600

    jagged arrays

--- arrays/arrays_2d.java
+++ arrays/arrays_2d.java
@@ -16,5 +16,22 @@ public class arrays_2d {
             for (int i = 0 ; i < numbers.length ; i++)
                   for (int j = 0 ; j < numbers[i].length ; j++)
                         number[i][j] = sc.nextInt() ;
+
+            // jagged array : In Java, a jagged array is a 2D array where each row can have a different number of columns.
+
+            int[][] jagged = new int[3][];  // 3 rows, but columns not set yet
+
+            jagged[0] = new int[2];  // first row has length 2
+            jagged[1] = new int[4];  // second row has length 4
+            jagged[2] = new int[3];  // third row has length 3
+
+            // Declare jagged array with 3 rows
+            int[][] jagged2 = new int[3][];
+
+            // Initialize each row with different size
+            jagged2[0] = new int[] {1, 2};
+            jagged2[1] = new int[] {3, 4, 5, 6};
+            jagged2[2] = new int[] {7, 8, 9};
+
       }
 }
