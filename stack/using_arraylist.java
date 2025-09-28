ee61adc5c0450a52690b2a82a1a3db8e261e8be5
Sayem <sayemsoftd@gmail.com>
2025-09-29 00:38:39 +0600

    stack using arraylist

---
+++ stack/using_arraylist.java
@@ -0,0 +1,57 @@
+/**
+ *    author:  Lunero
+ *    created: 29.09.2025 00:28:10
+**/
+import java.util.* ;
+
+public class using_arraylist {
+
+      static class Stack {
+            static ArrayList<Integer> list = new ArrayList<>() ;
+
+            public static boolean isEmpty() {
+                  return list.size() == 0 ;
+            }
+
+            public static void push (int data) {
+                  list.add (data) ;
+            }
+
+            public static int pop () {
+                  if (isEmpty())
+                        return -1 ;
+
+                  int data = list.get(list.size() - 1) ;
+
+                  list.remove (list.size() - 1) ;
+
+                  return data ;
+            }
+
+            public static int peek () {
+                  if (isEmpty())
+                        return -1 ;
+                  
+                  return list.get(list.size() - 1) ;
+            }
+      }
+
+      public static void main (String[] args) {
+            Scanner sc = new Scanner (System.in) ;
+
+            // stack implementation : array, arraylist, linkedlist
+            // arraylist
+            Stack s = new Stack () ;
+            s.push(1) ;
+            s.push(2) ;
+            s.push(3) ;
+            s.push(4) ;
+
+            while (!s.isEmpty()) {
+                  // System.out.println (s.peek()) ;
+                  // s.pop() ;
+
+                  System.out.println (s.pop()) ;
+            }
+      }
+}
