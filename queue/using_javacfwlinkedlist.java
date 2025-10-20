38a675185b20ed7fbebff18469653b43c60e05b6
Sayem <sayemsoftd@gmail.com>
2025-10-21 02:30:18 +0600

    Queue using Java Collection Framework

---
+++ queue/using_javacfwlinkedlist.java
@@ -0,0 +1,28 @@
+/**
+ *    author:  Lunero
+ *    created: 21.10.2025 02:19:57
+**/
+import java.util.* ;
+
+public class using_javacfwlinkedlist {
+      public static void main (String[] args) {
+            Scanner sc = new Scanner (System.in) ;
+
+            // Queue using Java Collection Framework
+            // Implementation 
+            Queue<Integer> q = new LinkedList<>() ;
+            // Character, Boolean, ...
+
+            q.add (1) ;
+            q.add (2) ;
+            q.add (3) ;
+            q.add (4) ;
+            q.add (5) ;
+
+            while (!q.isEmpty()) {
+                  System.out.println (q.peek()) ;
+                  q.remove() ;
+                  // System.out.println (q.remove()) ;
+            }
+      }
+}
